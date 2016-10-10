14a36c7e80f891b5360b6445e7035173f4069156
LeafarTHENERD <leafar.thenerd@hotmail.com>
2016-10-10 11:52:15 -0300

    update on euler method

--- list1/euler_h.m
+++ list1/euler_h.m
@@ -13,12 +13,12 @@
 %             				   Default h = 0.01
 
 function Y = euler_h(fun, y_inicial, t_inicial, t_final, h = 0.01)
-	Y(1) = y_inicial;
+	Y(1, :) = y_inicial;
 	T = t_inicial:h:t_final;
 	N = size(T)(2);
 
 	for k = 2:N
-		phi = feval(fun, T(k-1), Y(k-1),0);
-		Y(k) = Y(k-1) + h*phi;
+		phi = feval(fun, T(k-1), Y(k-1, :), 0);
+		Y(k, :) = Y(k-1, :) + h*phi;
 	end
 end
