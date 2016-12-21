9d4db92053a29574df4863f13ed1710f5fb3e8da
LeafarTHENERD <leafar.thenerd@hotmail.com>
2016-12-21 11:59:00 -0200

    Adding solutions to boundary problems using crank-nicolson method

---
+++ list3/crank_nicolson_lower_bound.m
@@ -0,0 +1,52 @@
+% function v = crank_nicolson_lower_bound(u_inicial, u_x_inicial, x_inicial, x_final, t_final, a, lambda, h)
+%
+% v = crank_nicolson_lower_bound(u_inicial, u_x_inicial, x_inicial, x_final, t_final, a, lambda, h)
+%
+% ESQUEMA CRANK-NICOLSON: resolve um PVC 
+%   u_t + a * u_x = 0 , 0 <= t <= t_final, x_inicial <= x <= x_final
+%   com u0(x)         = u_inicial
+%   com uxinicial(t)  = u_x_inicial
+%
+% DADOS:
+%   u_inicial                - funcao em t = 0
+%   u_x_inicial              - funcao em x = x_inicial
+%   x_inicial                - inicio intervalo do espaço
+%   x_final                  - fim intervalo do espaço
+%   t_final                  - tempo máximo onde se desejar computar
+%   a                        - constante do PVC
+%   lambda                   - parametro de espaçamento da malha
+%   h                        - parametro de espaçamento da malha
+%  
+
+function v = crank_nicolson_lower_bound(u_inicial, u_x_inicial, x_inicial, x_final, t_final, a, lambda, h)
+    x_m = x_inicial:h:x_final;
+
+    M_final = size(x_m)(2);
+    M_inicial = 2;
+    t_now = 1;
+    k = lambda * h;
+
+    % dados iniciais
+    for m = 1:M_final
+        v(t_now, m) = feval(@u_inicial, x_m(m));
+    end
+
+    % dados iniciais utilizando ftcs para obter o segundo nivel de computação requerido pelo leapfrog
+    t_new = t_now + 1;
+    for m = M_inicial:M_final-1
+        v(t_new, m) = v(t_now, m) - (1/2)*(a * lambda) * (v(t_now, m+1) - v(t_now, m-1));
+    end
+    v(t_new, M_final) = v(t_new, M_final-1);
+    t_now = t_new;
+    %M_inicial = M_inicial + 1;
+
+    for t = k:k:t_final-k
+        t_new = t_now + 1;
+        for m = M_inicial:M_final-1
+            v(t_new, m) = v(t_now - 1, m) - (a * lambda)*(v(t_now, m+1) - v(t_now, m-1));
+        end
+        M_inicial = M_inicial + 1;
+        v(t_new, M_final) = v(t_new, M_final-1);
+        t_now = t_new;
+    end
+end
---
+++ list3/ex3-5-1.m
@@ -0,0 +1,39 @@
+% Exercicio 3.5.1 - PVC := u_t + u_x = 0 em -1 <= x <= 1, 0 <= t <= 1
+%                          u(t, -1) = sin pi * (x - t)
+%                          u(0,  x) = sin pi * (x - t)
+x_inicial   = -1;
+x_final     = 1;
+t_final     = 1;
+a           = 1;
+hs          = [1/10 1/20 1/40];
+lambda      = 1;
+
+function u = u_minus_1(t)
+    u = sin(pi*(-1 - t));
+end
+
+function u = u_inicial(x)
+    u = sin(pi * x);
+end
+
+
+v = crank_nicolson_lower_bound(@u_inicial, @u_minus_1, x_inicial, x_final, t_final, a, lambda, h);
+
+x_m = x_inicial:h:x_final;
+t_n = 0:(lambda*h):t_final;
+
+fg = figure;
+hold on;
+
+x_m = x_inicial:h:x_final;
+t_n = 0:(lambda*h):t_final;
+
+xlabel('x_m');
+ylabel('t_n');
+zlabel('v_n_m');
+
+view(3);
+mesh(x_m, t_n, v);
+
+fprintf('Program paused. Press enter to exit.\n');
+pause;
