dd04382c90bc423292c632e68e8baa11fa890119
LeafarTHENERD <leafar.thenerd@hotmail.com>
2016-12-22 09:11:20 -0200

    That's all folks!

--- list3/examplePg-159.m
+++ list3/examplePg-159.m
@@ -2,45 +2,50 @@
 %                        u_0 (x) = tent function between -0.5 e 0.5
 %
 
-x_inicial  = 0;
-x_final    = 4;
+x_inicial  = -2;
+x_final    = 10;
 t_final    = 0.8;
 a          = 10;
-b		   = 0.1;
-mu		   = 1;
+b          = 0.1;
+mu         = 1;
 hs         = [1/20 1/30];
 
 function u = u_inicial(x)
-    if x >= 0 && x <= 0.5,
-        u = 1 - 2*x;
-    elseif x < 0 && x >= -0.5
-    	u = 1 + 2*x;
-%	if(abs(x) <= .5)
-%		u = sin(pi*x);
-    else
-    	u = 0;
-    end
+    u = max(1 - abs(2*x), 0);
 end
 
-%h = hs(3);
-%v = ftcs(@u_inicial, x_inicial, x_final, t_final, a, b, mu, h);
-
 h = hs(1);
-v = upwind(@u_inicial, x_inicial, x_final, t_final, a, b, mu, h);
+v = ftcs(@u_inicial, x_inicial, x_final, t_final, a, b, mu, h);
 
+%h = hs(2);
+%v = upwind(@u_inicial, x_inicial, x_final, t_final, a, b, mu, h);
 
-x_m = x_inicial:h:x_final;
-t_n = 0:(mu*h*h):t_final;
 
 fg = figure;
 hold on;
 
+x_m = x_inicial:h:x_final;
+t_n = 0:(mu*h*h):t_final;
+
+
+t = v(size(v)(1), :);
+
+p = plot(x_m, t);
+set(p, 'marker', 'o');
+set(p, 'color', [1 .25 .5]);
+set(p, 'markerSize', 5);
+
+p = plot(x_m, u_inicial(x_m));
+set(p, 'marker', '+');
+set(p, 'color', [1 .25 .5]);
+
+
 xlabel('x_m');
 ylabel('t_n');
-zlabel('v_n_m');
+%zlabel('v_n_m');
 
-view(3);
-mesh(x_m, t_n, v);
+%view(3);
+%mesh(x_m, t_n, v);
 
 fprintf('Program paused. Press enter to exit.\n');
 pause;
