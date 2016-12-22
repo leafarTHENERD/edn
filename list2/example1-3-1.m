e02461ceb6f7dfb28dc672b987f76aedf39e9dad
LeafarTHENERD <leafar.thenerd@hotmail.com>
2016-12-22 08:43:04 -0200

    Just console output clean up

--- list2/example1-3-1.m
+++ list2/example1-3-1.m
@@ -14,11 +14,11 @@ function u = u_inicial(x)
         u = 1 - abs(x);
     else
         u = 0;
-    endif
+    end
 end
 
 v = lax_friedrichs(@u_inicial, x_inicial, x_final, t_objetivo, a, lambda, h);
-t = v(size(v)(1), :)
+t = v(size(v)(1), :);
 
 fg = figure;
 hold on;
@@ -31,6 +31,7 @@ hold on;
 x_m = x_inicial:h:x_final;
 t_n = 0:(lambda*h):t_objetivo;
 
+view(3);
 mesh(x_m, t_objetivo, t);
 mesh(x_m, t_n, v);
 
